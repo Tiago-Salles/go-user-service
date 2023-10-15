0c1cdb26f8435b9e90e93125657ca05275870866
Tiago-Salles <tiagosalles20@gmail.com>
2023-10-15 12:40:49 +0000

    fix: replaced go.mod, feat: calling register user from main.go

--- src/main.go
+++ src/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	
+	"fmt"
+
+	"github.com/Tiago-Salles/go-user-service/src/domain/use_cases"
+
+)
+
 
 func main(){
 	fmt.Println("APP IS RUNNING")
+
+	useCase := use_cases.UserUseCase{}
+	user := useCase.Register()
+	fmt.Println(user.Name)
+	fmt.Println(user.Email)
 }
